e243a5173acbb6355fe2450e6dc248691f809531
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-02-08 23:44:46 +0600

    solve(digitize): add solution using string concatenation + unary plus

--- 8-kyu/digitize.js
+++ 8-kyu/digitize.js
@@ -16,6 +16,8 @@ const digitizeToString = n => n.toString().split("").reverse().map(Number);
 // Approach 3: Spread Operator + Map + Reverse
 const digitizeSpread = n => [...String(n)].reverse().map(Number);
 
+// Approach 4: String Concatenation + Split + Unary Plus
+const digitizeUnaryPlus = n => (n + '').split('').reverse().map(x => +x);
 
 
 // Test Outputs
@@ -24,3 +26,4 @@ console.log(digitizeSplitMap(0));       // [0]
 
 console.log(digitizeToString(35231));   // [1, 3, 2, 5, 3]
 console.log(digitizeSpread(35231));     // [1, 3, 2, 5, 3]
+console.log(digitizeUnaryPlus(35231));  // [1, 3, 2, 5, 3]
