aa00e1ae26b6e752e6436a42a310824db85fda2d
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-02-12 22:00:59 +0600

    solve(findNeedle): add ternary operator fallback

--- 8-kyu/findNeedle.js
+++ 8-kyu/findNeedle.js
@@ -26,7 +26,13 @@ function findNeedleFindIndex(haystack) {
     return `found the needle at position ${index}`;
 }
 
-
+// Approach 4: indexOf + ternary operator
+function findNeedleTernary(haystack) {
+    const index = haystack.indexOf("needle");
+    return (index !== -1)
+        ? `found the needle at position ${index}`
+        : "needle not found";
+}
 
 // Test Outputs
 const testArray1 = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
@@ -37,3 +43,4 @@ console.log(findNeedleTemplate(testArray1));  // "found the needle at position 5
 console.log(findNeedleFindIndex(testArray1)); // "found the needle at position 5"
 
 console.log(findNeedleTernary(testArray1));   // "found the needle at position 5"
+console.log(findNeedleTernary(testArray2));   // "needle not found"
