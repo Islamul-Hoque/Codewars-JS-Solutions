5bee39275dec9964ab1a6aaafa3a6d477110ea04
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-02-19 23:59:09 +0600

    solve(doubleArrayValues): add for loop solution

--- 8-kyu/doubleArrayValues.js
+++ 8-kyu/doubleArrayValues.js
@@ -6,8 +6,18 @@ function mapsMap(x) {
 // Approach 2: arrow function + map
 const mapsArrow = x => x.map(n => n * 2);
 
+// Approach 3: for loop
+function mapsForLoop(x) {
+    let result = [];
+    for (let i = 0; i < x.length; i++) {
+        result.push(x[i] * 2);
+    }
+    return result;
+}
+
 
 
 // Test Outputs
 console.log(mapsMap([1, 2, 3]));       // [2, 4, 6]
 console.log(mapsArrow([4, 1, 1, 1, 4])); // [8, 2, 2, 2, 8]
+console.log(mapsForLoop([2, 2, 2, 2, 2])); // [4, 4, 4, 4, 4]
