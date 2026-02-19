81664cce6f1ebdf000d34f0771de9299a11e32de
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-02-19 23:58:28 +0600

    solve(doubleArrayValues): add arrow function variation with map

--- 8-kyu/doubleArrayValues.js
+++ 8-kyu/doubleArrayValues.js
@@ -1,3 +1,13 @@
-function maps(x) {
-  return x.map(n => n * 2);
+// Approach 1: map method
+function mapsMap(x) {
+    return x.map(n => n * 2);
 }
+
+// Approach 2: arrow function + map
+const mapsArrow = x => x.map(n => n * 2);
+
+
+
+// Test Outputs
+console.log(mapsMap([1, 2, 3]));       // [2, 4, 6]
+console.log(mapsArrow([4, 1, 1, 1, 4])); // [8, 2, 2, 2, 8]
