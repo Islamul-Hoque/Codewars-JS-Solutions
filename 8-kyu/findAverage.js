c33a08afd236a623b39b07c549c87291b9acee05
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-02-21 23:56:17 +0600

    solve(findAverage): add solution using for-of loop

--- 8-kyu/findAverage.js
+++ 8-kyu/findAverage.js
@@ -8,8 +8,20 @@
 // Approach 1: arrow + reduce (concise)
 const findAverageArrow = arr => arr.length ? arr.reduce((a, b) => a + b, 0) / arr.length : 0;
 
+// Approach 2: for...of loop
+function findAverageForOf(array) {
+    if (array.length == 0) return 0;
+    let result = 0;
+    for (let num of array) {
+        result += num;
+    }
+    return result / array.length;
+}
+
 
 
 // Test Outputs
 console.log(findAverageArrow([1, 2, 3]));       // 2
 console.log(findAverageArrow([100]));           // 100
+
+console.log(findAverageForOf([5, 10, 15]));     // 10
