111ef64bf192b89e769aaac43e88346b2975807d
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-03-08 23:48:32 +0600

    solve(whoLikesIt): implement solution using switch statement

--- 8-kyu/whoLikesIt.js
+++ 8-kyu/whoLikesIt.js
@@ -24,6 +24,18 @@ const likesTernary = names =>
     names.length === 3 ? `${names[0]}, ${names[1]} and ${names[2]} like this` :
     `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 
+// Approach 2: Switch statement
+function likesSwitch(names) {
+    switch (names.length) {
+        case 0: return "no one likes this";
+        case 1: return `${names[0]} likes this`;
+        case 2: return `${names[0]} and ${names[1]} like this`;
+        case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+        default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    }
+}
+
 
 // Test Outputs
 console.log(likesTernary([]));                          // "no one likes this"
+console.log(likesSwitch(["Peter"]));                    // "Peter likes this"
